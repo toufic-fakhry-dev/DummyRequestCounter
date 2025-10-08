1978002079e728c0a468e36283493e9f2290ad97
marc <marcochoucair@hotmail.com>
2025-10-08 18:30:52 +0300

    Fix Flake8 style issues

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,7 @@ from app.app import app  # make sure this matches your FastAPI app location
 
 client = TestClient(app)
 
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
