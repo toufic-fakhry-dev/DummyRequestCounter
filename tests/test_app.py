c126d658b226ca0ab23dcf28be25015f00c530c7
marc <marcochoucair@hotmail.com>
2025-10-08 18:54:17 +0300

    Fix import path for tests2.0

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,9 @@ from fastapi.testclient import TestClient
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+
 from app.app import app
+
 
 client = TestClient(app)
 
