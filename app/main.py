9a3883ec96c80aebc5348867c32543569d30f913
Elie Rhayem <rhayemelie5@gmail.com>
2025-10-08 19:23:16 +0300

    Add full CI/CD pipeline

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import os
 
 app = FastAPI()
 
-redis_host = os.getenv("REDIS_HOST", "redis")
+redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = int(os.getenv("REDIS_PORT", 6379))
 r = redis.Redis(host=redis_host, port=redis_port)
 
