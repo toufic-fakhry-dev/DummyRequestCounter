c34cdf7861c019b5198217253f5decf9d45d56ea
fatima el harake <fatima.harake@net.usj.edu.lb>
2025-10-14 21:02:09 +0300

    Fix flake8 spacing: E302/E305/W293/E303/W292

--- app/app.py
+++ app/app.py
@@ -1,23 +1,22 @@
 from flask import Flask
-import redis
 import os
+import redis
 
+# Create Flask app
 app = Flask(__name__)
 
+# Redis config (use environment variables set in docker-compose)
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
-APP_PORT = int(os.getenv("APP_PORT", "8000"))
 
 @app.route("/")
 def hello():
     hits = r.incr("hits")
     return f"Hello! This page has been visited {hits} times."
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=APP_PORT, debug=True)
-
-
-
-    
+    # For local debugging (not used inside Docker)
+    app.run(host="0.0.0.0", port=8000, debug=True)
