2d7766ece6adbd19cfc363cc2f687252bd35b567
marilyn <marilyn.aboutayeh@net.usj.edu.lb>
2025-10-14 20:37:55 +0300

    Fix linting issues detected by flake8

--- app/app.py
+++ app/app.py
@@ -5,17 +5,25 @@ import uvicorn
 
 app = FastAPI()
 
-REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+
+# Choose Redis host based on environment
+if os.getenv("CI") or os.getenv("DOCKER_ENV"):
+    REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+else:
+    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
-REDIS_DB   = int(os.getenv("REDIS_DB", "0"))
+REDIS_DB = int(os.getenv("REDIS_DB", "0"))
 
 redis = Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
 
+
 @app.get("/")
 def hello():
-    redis.incr('hits')
-    hits = redis.get('hits')
+    redis.incr("hits")
+    hits = redis.get("hits")
     return f"Hello! This page has been visited {hits} times."
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
