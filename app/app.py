00ca4799e7b4fed4290626fb61e13c0be2ba3ddb
youssef <youssef.saade@net.usj.edu.lb>
2025-10-15 10:03:36 +0300

    Ignore Bandit B104 warning with #nosec

--- app/app.py
+++ app/app.py
@@ -18,4 +18,4 @@ def hello():
     return f"Hello! This page has been visited {hits} times."
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # nosec B104
