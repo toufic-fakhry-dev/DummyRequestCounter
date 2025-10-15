c98fd01433313c672ce3f7747e1eb3936571c2fd
toufic.f <t.fakhry@ob-soft.com>
2025-10-15 10:23:33 +0300

    adding tests

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_app_request_counting():
+    _status_code = 200
+    _content = "Hello! This page has been visited"
+    response = client.get("/")
+
+    status_code = response.status_code
+    content = response.text
+
+    assert status_code == _status_code
+    assert _content in content
