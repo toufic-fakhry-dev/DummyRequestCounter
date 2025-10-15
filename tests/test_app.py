10a29757728ceb788a2bfb1ece6942bb16d6d7a0
“myriambraidy” <myriam-braidy1@gmail.com>
2025-10-15 09:53:52 +0300

    fixing the run tests issue

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,8 @@
 from fastapi.testclient import TestClient
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.app import app
+
 
 client = TestClient(app)
 
